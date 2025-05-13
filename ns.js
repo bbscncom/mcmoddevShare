13fa9b8b262c2bf244de662146bcd4c2b4949c82
bbscncom <bbscncom>
2025-05-13 18:24:56 +0800

    2025/05/13 周二

---
+++ ns.js
@@ -0,0 +1,9 @@
+module.exports = {
+    cname: {
+        "rundocs.io": "rundocs.github.io",
+        /* start */
+    },
+    txt: {
+        "rundocs.io": "google-site-verification=bnnUUSDymPpDsVxL4OVeq9IAWSHnsm_1G2Zw1K9E8R4",
+    }
+}
